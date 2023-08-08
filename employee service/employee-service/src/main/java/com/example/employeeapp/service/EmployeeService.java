5c8b0b92041eb7628e12d3267e58dcff90bf7a50
= <roshanshoti100@gmail.com>
2023-08-07 21:40:06 -0400

    successfully making rest call to the address microservice using restemplate

--- employee service/employee-service/src/main/java/com/example/employeeapp/service/EmployeeService.java
+++ employee service/employee-service/src/main/java/com/example/employeeapp/service/EmployeeService.java
@@ -1,5 +1,4 @@
 package com.example.employeeapp.service;
-
 import com.example.employeeapp.entity.Employee;
 import com.example.employeeapp.repository.EmployeeRepo;
 import com.example.employeeapp.response.AddressResponse;
@@ -7,28 +6,27 @@ import com.example.employeeapp.response.EmployeeResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmployeeService {
     private EmployeeRepo employeeRepo;
-
-    @Autowired
     private ModelMapper modelMapper;
 
+    private RestTemplate restTemplate;
     @Autowired
-    public EmployeeService(EmployeeRepo employeeRepo) {
+    public EmployeeService(EmployeeRepo employeeRepo, ModelMapper modelMapper, RestTemplate restTemplate) {
         this.employeeRepo = employeeRepo;
-
+        this.modelMapper = modelMapper;
+        this.restTemplate = restTemplate;
     }
     public EmployeeResponse getEmployeeById(int theId){
 
-        //addressResponse -> set data by making a rest api call
-        AddressResponse addressResponse = new AddressResponse();
-
         //convert employee -->employee response
         Employee employee = employeeRepo.findById(theId).get();
-
         EmployeeResponse employeeResponse = modelMapper.map(employee, EmployeeResponse.class);
+        //                                                                        url                                             response            uri variable
+        AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8081/address-app/api/address/{theId}",AddressResponse.class, theId);
 
         employeeResponse.setAddressResponse(addressResponse);
 

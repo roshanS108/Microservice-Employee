268d29bf5f7be7a4e3144d633045c74661a57cab
= <roshanshoti100@gmail.com>
2023-08-07 21:50:52 -0400

    injecting the base url from properties file

--- employee service/employee-service/src/main/java/com/example/employeeapp/service/EmployeeService.java
+++ employee service/employee-service/src/main/java/com/example/employeeapp/service/EmployeeService.java
@@ -5,15 +5,18 @@ import com.example.employeeapp.response.AddressResponse;
 import com.example.employeeapp.response.EmployeeResponse;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
 @Service
 public class EmployeeService {
-    private EmployeeRepo employeeRepo;
-    private ModelMapper modelMapper;
+    private final EmployeeRepo employeeRepo;
+    private final ModelMapper modelMapper;
+    private final RestTemplate restTemplate;
 
-    private RestTemplate restTemplate;
+    @Value("${addressservice.base.url}")
+    private String addressBaseUrl;
+
     @Autowired
     public EmployeeService(EmployeeRepo employeeRepo, ModelMapper modelMapper, RestTemplate restTemplate) {
         this.employeeRepo = employeeRepo;
@@ -21,13 +24,11 @@ public class EmployeeService {
         this.restTemplate = restTemplate;
     }
     public EmployeeResponse getEmployeeById(int theId){
-
         //convert employee -->employee response
         Employee employee = employeeRepo.findById(theId).get();
         EmployeeResponse employeeResponse = modelMapper.map(employee, EmployeeResponse.class);
         //                                                                        url                                             response            uri variable
-        AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8081/address-app/api/address/{theId}",AddressResponse.class, theId);
-
+        AddressResponse addressResponse = restTemplate.getForObject(addressBaseUrl + "/address/{id}",AddressResponse.class, theId);
         employeeResponse.setAddressResponse(addressResponse);
 
         return employeeResponse;

210514268a3535babfe83554a2aab1a0c698c3d4
= <roshanshoti100@gmail.com>
2023-08-19 23:18:53 -0400

    creating bean for address client

--- employee-service-2-new/src/main/java/com/example/employeeapp/config/EmployeeAppConfig.java
+++ employee-service-2-new/src/main/java/com/example/employeeapp/config/EmployeeAppConfig.java
@@ -1,6 +1,9 @@
 package com.example.employeeapp.config;
 
+import com.example.employeeapp.feignclient.AddressClient;
+import com.example.employeeapp.response.AddressResponse;
 import org.modelmapper.ModelMapper;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -16,4 +19,18 @@ public class EmployeeAppConfig {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
+    @Bean
+    public AddressClient addressClient(){
+        return new AddressClient() {
+            @Override
+            public AddressResponse getAddressByEmployeeId(int theId) {
+                return new AddressResponse();
+            }
+        };
+    }
+
+
+
+
 }

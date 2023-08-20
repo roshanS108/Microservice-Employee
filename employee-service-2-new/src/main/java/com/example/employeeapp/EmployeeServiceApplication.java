e71ec43be8423e234a19d9ccf143859e9bb2624f
= <roshanshoti100@gmail.com>
2023-08-19 23:18:20 -0400

    enabling external serve

--- employee-service-2-new/src/main/java/com/example/employeeapp/EmployeeServiceApplication.java
+++ employee-service-2-new/src/main/java/com/example/employeeapp/EmployeeServiceApplication.java
@@ -1,9 +1,13 @@
 package com.example.employeeapp;
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
+@EnableFeignClients("com.example.employeeapp.feignclient")
+@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {

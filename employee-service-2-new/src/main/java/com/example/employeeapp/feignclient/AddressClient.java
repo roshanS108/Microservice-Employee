5b84c0021ef411a4330adbed5ff02d16f2c27e9a
= <roshanshoti100@gmail.com>
2023-08-19 21:45:07 -0400

    introducing feign client for rest call

--- employee-service-2-new/src/main/java/com/example/employeeapp/feignclient/AddressClient.java
+++ employee-service-2-new/src/main/java/com/example/employeeapp/feignclient/AddressClient.java
@@ -1,2 +1,15 @@
-package com.example.employeeapp.feignclient;public interface AddressClient {
+package com.example.employeeapp.feignclient;
+
+import com.example.employeeapp.response.AddressResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient
+public interface AddressClient {
+
+    @GetMapping("/address/{id}")
+    AddressResponse getAddressByEmployeeId(@PathVariable("id") int id);
+
+
 }
